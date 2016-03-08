a562b93172351c2095cdf05f4f5abf4fe473ef3a
Emily Quill <emilyquill@outlook.com>
2016-03-08 11:37:28 +1100

    Ability to take turns, place moves and paint board

--- js/main.js
+++ js/main.js
@@ -4,11 +4,18 @@ var Board = {
             ['','','']],
   moves: 0,
   placeMove: function(event) {
-    console.log(event.target);
-        Board.moves += 1;
-
-    // 1. Update board array with the move [row,col]
-    // 2. Refresh board display (based on the current array)
+    var moveRow = event.target.dataset.row;
+    var moveCol = event.target.dataset.col;
+    console.log(moveRow, moveCol);
+    var moveLocation = Board.squares[moveRow][moveCol];
+    if (moveLocation === '') { // space free
+      Board.squares[moveRow][moveCol] = (Board.whoseTurn());
+      Board.moves += 1;
+      console.log("It's player " + Board.whoseTurn() + "'s turn now");
+    } else {
+      alert("Nice try.. that place is taken..");
+    }
+    Board.displayBoard();
     // 3. Calculate if there is a winner
 
   },
@@ -17,14 +24,25 @@ var Board = {
   checkWinDiagonal: function() {},
 
   whoseTurn: function() {
-    if (moves %2 === 0) {
+    if (Board.moves %2 === 0) {
       return 'X';
     } else {
       return 'O';
     }
   },
 
-  displayBoard: function() {}
+  displayBoard: function() {
+    for (var i = 0; i < Board.squares.length ; i++) {
+      for (var j = 0; j< Board.squares[0].length; j++) {
+      var move = Board.squares[i][j];
+      $('div[data-row="' +[i]+'"][data-col="' +[j]+'"]').text(move);
+      }
+    }
+
+
+      //  Board.squares[moveRow][moveCol]
+  },
+  displayTurn: function() {},
 
 };
 
